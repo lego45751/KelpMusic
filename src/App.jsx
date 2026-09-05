aa3550b75bc86ce08f4ff4a43d1fdbe04f9bbebc
lego45751 <lego45751@gmail.com>
2026-09-05 22:48:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import React, { useEffect, useMemo, useRef, useState, useCallback } from "react";
 import {
   Upload, Music, ListMusic, Download, Plus, Play, Pause, ChevronLeft,
